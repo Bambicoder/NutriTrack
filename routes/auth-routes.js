33a185e07339352e9347c20f2969129a808bc5ff
Bambicoder <mishraanushka543@gmail.com>
2024-03-31 20:57:59 +0530

    made changes in auth-routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,8 +6,8 @@ const pool = require('../data/database');
 const dotenv = require("dotenv").config();
 
 passport.use(new GoogleStrategy({
-    clientID: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback: true
   },
